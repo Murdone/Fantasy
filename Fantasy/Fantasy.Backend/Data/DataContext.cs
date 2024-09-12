9623ed6ce7a0265901a3d168226070f15bc6bd68
Percibal Vasquez <vasquez.percibal@gmail.com>
2024-09-12 15:29:08 -0300

    Add team entity

--- Fantasy/Fantasy.Backend/Data/DataContext.cs
+++ Fantasy/Fantasy.Backend/Data/DataContext.cs
@@ -10,10 +10,22 @@ public class DataContext : DbContext
     }
 
     public DbSet<Country> Countries { get; set; }
+    public DbSet<Team> Teams { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();//validando y creando un indice
+        modelBuilder.Entity<Team>().HasIndex(x => new { x.CountryId, x.Name }).IsUnique(); //indice unico pero compuesto este indice es por contry id y el nombre
+        DisableCascadingDelete(modelBuilder);
+    }
+
+    private void DisableCascadingDelete(ModelBuilder modelBuilder)
+    {
+        var relationships = modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys());
+        foreach (var relationship in relationships)
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
     }
 }
---
+++ Fantasy/Fantasy.shared/Entities/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fantasy.shared.Entities
+{
+    public class Team
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public string? Image { get; set; }
+        public Country Country { get; set; } = null!;
+        public int CountryId { get; set; }
+    }
+}
